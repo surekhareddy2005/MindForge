a653c2075e243405f40a1bcb57b22fde1d2b98fe
surekha reddy <surekhareddy5002@gmail.com>
2026-06-27 11:17:42 +0530

    Add missing seed.js

---
+++ server/src/utils/seed.js
@@ -0,0 +1,8 @@
+export const seedUsers = async () => {
+  console.log("seedUsers: seeding is disabled.");
+  return [];
+};
+
+export const seedCourses = async (users = []) => {
+  console.log("seedCourses: seeding is disabled.");
+};
